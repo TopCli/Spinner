666016fff56a214e6b7a3a6a4f5b7ad180c254d7
Malaj Alexandre <alexandre.malaj@gmail.com>
2019-05-02 11:02:26 +0200

    chore: first draft

---
+++ index.js
@@ -0,0 +1,55 @@
+const cliSpinners = require("cli-spinners");
+const Spinner = require("./src/Spinner.js");
+
+/* eslint-disable no-loop-func */
+async function startAll(array) {
+    let started = 0;
+    let finished = 0;
+    let succeeded = 0;
+    let failed = 0;
+    function writeRecap() {
+        const col = process.stdout.columns;
+        const recap = `${finished} / ${array.length} : with ${failed} failed`;
+        const displayRecap = recap.length > col ? recap.slice(0, col) : recap;
+        process.stdout.moveCursor(0, 2);
+        process.stdout.clearLine();
+        process.stdout.write(displayRecap);
+        process.stdout.moveCursor(-recap.length, -2);
+    }
+
+    Spinner.emitter.on("start", () => {
+        started++;
+        if (started === array.length) {
+            writeRecap();
+        }
+    });
+    Spinner.emitter.on("success", () => {
+        finished++;
+        succeeded++;
+        writeRecap();
+
+        if (finished === array.length) {
+            process.stdout.moveCursor(0, 3);
+        }
+    });
+    Spinner.emitter.on("fail", () => {
+        finished++;
+        failed++;
+        writeRecap();
+
+        if (finished === array.length) {
+            process.stdout.moveCursor(0, 3);
+        }
+    });
+
+    // console.log();
+    // console.log(`${finished} / ${array.length}`);
+    const results = await Promise.all(array);
+
+    return results;
+}
+
+module.exports = {
+    startAll,
+    Spinner
+};
---
+++ src/spinner.class.js
@@ -0,0 +1,198 @@
+// Require Third-party Dependencies
+const is = require("@slimio/is");
+const cliSpinners = require("cli-spinners");
+const SafeEmitter = require("@slimio/safe-emitter");
+
+// CONSTANT
+const DEFAULT_SPINNER = "dots";
+
+const SPINNER = Symbol("spinner");
+const PREFIX_TEXT = Symbol("prefixText");
+const TEXT = Symbol("text");
+
+/**
+ * @class Spinner
+ *
+ * @property {String} prefixText
+ */
+class Spinner {
+    /**
+     * @constructor
+     * @memberof #Spinner
+     * @param {Object} options options
+     */
+    constructor(options = Object.create(null)) {
+        this.emitter = new SafeEmitter();
+        this.spinner = options.spinner;
+        this.prefixText = options.prefixText;
+        this.text = is.string(options.text) ? options.text : "";
+        this.started = false;
+
+        this.stream = process.stdout;
+        this.emitter.once("start").then(() => {
+            this.spinnerPos = Spinner.count;
+            Spinner.count++;
+
+            return void 0;
+        });
+    }
+
+    /**
+     * @public
+     * @memberof Spinner#
+     * @param {String} value text value
+     */
+    set text(value) {
+        if (!is.string(value)) {
+            throw new TypeError("text must be a type of string");
+        }
+        this[TEXT] = value;
+    }
+
+    /**
+     *
+     */
+    get text() {
+        return this[TEXT];
+    }
+
+    /**
+     * @public
+     * @memberof Spinner#
+     * @param {String} value prefixText value
+     */
+    set prefixText(value) {
+        this[PREFIX_TEXT] = is.string(value) ? `${value} - ` : "";
+    }
+
+    /**
+     *
+     */
+    get prefixText() {
+        return this[PREFIX_TEXT];
+    }
+
+    /**
+     * @public
+     * @memberof Spinner#
+     * @param {Object|String} value text value
+     */
+    set spinner(value) {
+        if (is.plainObject(value)) {
+            if (is.nullOrUndefined(value.frames)) {
+                throw new Error("Spinner object must have a frames property");
+            }
+            if (is.nullOrUndefined(value.interval)) {
+                throw new Error("Spinner object must have an interval property");
+            }
+            this[SPINNER] = value;
+        }
+
+        if (process.platform === "win32") {
+            this[SPINNER] = cliSpinners[DEFAULT_SPINNER];
+        }
+        else if (is.nullOrUndefined(value)) {
+            this[SPINNER] = cliSpinners.dots;
+        }
+        else if (is.string(value)) {
+            if (cliSpinners[value]) {
+                this[SPINNER] = cliSpinners[value];
+            }
+            else {
+                throw new Error(`There is no built-in spinner named '${value}'. See "cli-spinners" from sindresorhus for a full list.`);
+            }
+        }
+        else {
+            throw new TypeError("spinner must be a type of string|object|undefined");
+        }
+    }
+
+    /**
+     *
+     */
+    get spinner() {
+        return this[SPINNER];
+    }
+
+    /**
+     * @method start
+     * @memberof Spinner#
+     * @param {String} text text
+     *
+     * @return {void}
+     */
+    start(text) {
+        this.started = true;
+        this.emitter.emit("start");
+        Spinner.emitter.emit("start");
+        let frameIndex = 0;
+
+        const terminalCol = this.stream.columns;
+
+        const { frames } = this.spinner;
+        const frame = frames[frameIndex];
+        frameIndex = ++frameIndex < frames.length ? frameIndex : 0;
+
+        const defaultRaw = `${frame} ${this.prefixText}${this.text}`;
+        const displayRaw = defaultRaw.length > terminalCol ? defaultRaw.slice(0, terminalCol) : defaultRaw;
+        console.log(displayRaw);
+        this.interval = setInterval(() => {
+            // move terminal cursor to line
+            const moveCursorPos = Spinner.count - this.spinnerPos;
+            this.stream.moveCursor(0, -moveCursorPos);
+            this.stream.clearLine();
+            const terminalCol = this.stream.columns;
+
+            const { frames } = this.spinner;
+            const frame = frames[frameIndex];
+            frameIndex = ++frameIndex < frames.length ? frameIndex : 0;
+
+            const defaultRaw = `${frame} ${this.prefixText}${this.text}`;
+            const displayRaw = defaultRaw.length > terminalCol ? defaultRaw.slice(0, terminalCol) : defaultRaw;
+            this.stream.write(displayRaw);
+            this.stream.moveCursor(-displayRaw.length, 0);
+
+            // move terminal cursor to the end
+            this.stream.moveCursor(0, moveCursorPos);
+        }, this.spinner.interval);
+
+        return this;
+    }
+
+    /**
+     * @method succeed
+     * @memberof Spinner#
+     * @param {String} text text
+     *
+     * @return {void}
+     */
+    succeed(text = "Success") {
+        if (this.started === false) {
+            return;
+        }
+        clearInterval(this.interval);
+        this.interval = null;
+        Spinner.emitter.emit("success", this);
+    }
+
+    /**
+     * @method fail
+     * @memberof Spinner#
+     * @param {String} text text
+     *
+     * @return {void}
+     */
+    fail(text = "Fail") {
+        if (this.started === false) {
+            return;
+        }
+        clearInterval(this.interval);
+        this.interval = null;
+        Spinner.emitter.emit("fail", this);
+    }
+}
+
+Spinner.count = 0;
+Spinner.emitter = new SafeEmitter();
+
+module.exports = Spinner;
